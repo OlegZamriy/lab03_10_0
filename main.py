2fe50845c44759d331bf6f68287c99f6c98b120d
lysenko_v <lysenko_v@itstep.academy>
2023-10-03 19:53:09 +0300

    1

---
+++ main.py
@@ -0,0 +1,8 @@
+def display_quote(author):
+    quote = "Don't let the noise of others' opinions\n" \
+            "drown out your own inner voice."
+    formatted_quote = f'"{quote}"\n{author}'
+    print(formatted_quote)
+
+author = "Steve Jobs"
+display_quote(author)
