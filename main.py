dd38e6a7037e7e147e0558654b1f31e84f4fabe5
lysenko_v <lysenko_v@itstep.academy>
2023-10-03 20:30:54 +0300

    1

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-def find_max_of_four(num1, num2, num3, num4):
-    return max(num1, num2, num3, num4)
+def sum_numbers_in_range(start, end):
+    total = 0
+    for number in range(start, end + 1):
+        total += number
+    return total
 
-num1 = float(input("Введіть перше число: "))
-num2 = float(input("Введіть друге число: "))
-num3 = float(input("Введіть третє число: "))
-num4 = float(input("Введіть четверте число: "))
+start = int(input("Введіть початкове число: "))
+end = int(input("Введіть кінцеве число: "))
 
-max_number = find_max_of_four(num1, num2, num3, num4)
-print(f"Максимальне число: {max_number}")
+result = sum_numbers_in_range(start, end)
+print(f"Сума чисел у діапазоні від {start} до {end}: {result}")
