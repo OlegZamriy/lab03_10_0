c9be9826e4926b35a21dd1f5a82210e79b105209
lysenko_v <lysenko_v@itstep.academy>
2023-10-03 20:12:21 +0300

    1

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-def draw_line():
-    length = int(input("Введіть довжину лінії: "))
-    direction = input("Введіть напрямок ('horizontal' або 'vertical'): ")
-    character = input("Введіть символ: ")
+def find_max_of_four(num1, num2, num3, num4):
+    return max(num1, num2, num3, num4)
 
-    if direction == 'horizontal':
-        line = character * length
-    elif direction == 'vertical':
-        line = '\n'.join(character for _ in range(length))
-    else:
-        print("Неправильний напрямок. Використовуйте 'horizontal' або 'vertical'.")
-        return
+num1 = float(input("Введіть перше число: "))
+num2 = float(input("Введіть друге число: "))
+num3 = float(input("Введіть третє число: "))
+num4 = float(input("Введіть четверте число: "))
 
-    print(line)
-
-draw_line()
+max_number = find_max_of_four(num1, num2, num3, num4)
+print(f"Максимальне число: {max_number}")
